ccfc97b8568996db17e1f13f7cc9d5b6356dbcd0
ryanadams <rcoleman.adams@gmail.com>
2022-04-06 21:01:45 -0500

    commit tree 12d8dcaac018658e2983361a299ba820c79bbf8a

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution here
+const findMatching = (array, string) => array.filter(element=>element.toLowerCase().includes(string.toLowerCase()));
+
+function fuzzyMatch(array,string){
+    return array.filter(element=>element.substring(0,2).toLowerCase() == string.toLowerCase().substring(0,2));
+};
+
+const matchName = (array, string) => array.filter(element=>element.name.toLowerCase().includes(string.toLowerCase()));
